d3b5c5d8b4de4508b1ce360c20b3f0797914ec63
Sebastian <sebastian.jarling@gmx.de>
2019-03-13 23:36:32 +0100

    Creating TankAIController and get Output log about it

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,23 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay()
+{
+	auto ControlledTank = GetControlledTank();
+	if (!ControlledTank)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController not possessing a tank"))
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("PlayerController possessing %s"), *ControlledTank->GetName())
+	}
+	Super::BeginPlay();
+	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"))
+}
+
+ATank* ATankPlayerController::GetControlledTank() const 
+{
+	return Cast<ATank>(GetPawn());
+}
